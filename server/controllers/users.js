9e71b067937576c76917ae39b1c8ca3da35390e1
hadad <hadadus@gmail.com>
2019-04-11 19:53:16 +0200

    [Chore ] ignore eslint for sequelize generated code

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,16 +1,26 @@
 import { User } from '../models/index';
 
 class Users {
-  async static create (req, res) {
+  /**
+   * @param  {} req
+   * @param  {} res
+   * @param  {} {try{awaitUser.create(req.user
+   * @param  {} ;}catch(error
+   * @param  {} {returnres.status(400
+   * @param  {400} .json({status
+   * @param  {'errormessage'} message
+   * @param  {} }
+   */
+  static async create(req, res) {
     try {
       await User.create(req.user);
     } catch (error) {
       return res.status(400).json({
-      status: 400,
-      message: 'error message',
-    })
+        status: 400,
+        message: 'error message',
+      });
     }
   }
 }
 
-export default Users
+export default Users;
