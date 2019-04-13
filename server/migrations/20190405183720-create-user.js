fa2e25981a859f1c1b9b1929f44857f20490e184
Gisele Iradukunda <vera@Giseles-MacBook-Pro.local>
2019-04-13 21:47:11 +0200

    165020126 implement the fetch average rating of an article endpoint

--- server/migrations/20190405183720-create-user.js
+++ server/migrations/20190405183720-create-user.js
@@ -1,5 +1,6 @@
 
 module.exports = {
+<<<<<<< HEAD
 	up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
 		id: {
 			allowNull: false,
@@ -40,4 +41,41 @@ module.exports = {
 		}
 	}),
 	down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
+=======
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    bio: {
+      type: Sequelize.TEXT
+    },
+    image: {
+      type: Sequelize.STRING
+    },
+    following: {
+      type: Sequelize.BOOLEAN
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
+>>>>>>> 165020126 implement the fetch average rating of an article endpoint
 };
