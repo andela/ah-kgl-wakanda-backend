a9edaee009ab4acfdb1dbf25d4837803a231ebca
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-04-15 12:11:06 +0200

    [Feature #165020121] refactor async DB queryand the DB Schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,38 +1,38 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-    },
-    provider: DataTypes.STRING,
-    bio: DataTypes.TEXT,
-    image: {
-      type: DataTypes.STRING,
-    },
-    following: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        min: 8
-      }
-    },
-  }, {});
-  User.associate = (models) => {
-    User.hasMany(models.Article, {
-      foreignKey: 'userId',
-      sourceKey: 'id'
-    });
-    User.hasMany(models.Comment);
-  };
+	const User = sequelize.define('User', {
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		email: {
+			type: DataTypes.STRING,
+		},
+		provider: DataTypes.STRING,
+		bio: DataTypes.TEXT,
+		image: {
+			type: DataTypes.STRING,
+		},
+		following: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			validate: {
+				min: 8
+			}
+		},
+	}, {});
+	User.associate = (models) => {
+		User.hasMany(models.Article, {
+			foreignKey: 'userId',
+			sourceKey: 'id'
+		});
+		User.hasMany(models.Comment);
+	};
 
-  return User;
+	return User;
 };
