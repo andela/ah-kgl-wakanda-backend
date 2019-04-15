d2278f240e33567c86a1aeb94004ef95bf8803d8
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-04-15 18:23:49 +0200

    [Feature #165020121] Update .travis.yml to support DB seeders

--- server/migrations/20190405183720-create-user.js
+++ server/migrations/20190405183720-create-user.js
@@ -1,43 +1,43 @@
 
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    username: {
-      type: Sequelize.STRING
-    },
-    email: {
-      type: Sequelize.STRING,
-      unique:true
-    },
-    provider: {
-      type: Sequelize.STRING,
-      defaultValue: 'local',
-    },
-    bio: {
-      type: Sequelize.TEXT
-    },
-    image: {
-      type: Sequelize.STRING
-    },
-    following: {
-      type: Sequelize.BOOLEAN
-    },
-    password: {
-      type: Sequelize.STRING
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  }),
-  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
+	up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER
+		},
+		username: {
+			type: Sequelize.STRING
+		},
+		email: {
+			type: Sequelize.STRING,
+			unique: true
+		},
+		provider: {
+			type: Sequelize.STRING,
+			defaultValue: 'local',
+		},
+		bio: {
+			type: Sequelize.TEXT
+		},
+		image: {
+			type: Sequelize.STRING
+		},
+		following: {
+			type: Sequelize.BOOLEAN
+		},
+		password: {
+			type: Sequelize.STRING
+		},
+		createdAt: {
+			allowNull: false,
+			type: Sequelize.DATE
+		},
+		updatedAt: {
+			allowNull: false,
+			type: Sequelize.DATE
+		}
+	}),
+	down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
 };
