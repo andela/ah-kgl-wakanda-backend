a72ebe710698eed0a0cf753d8be223d880dcf0da
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-04-16 15:37:39 +0200

    [Feature #165020125] Add validation middleware for rate and slug

--- server/routes/api/ratings.js
+++ server/routes/api/ratings.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import Ratings from '../../controllers/ratings';
+import Validation from '../../middlewares/validations/ratings';
 
 const router = express.Router();
 
-router.post('/articles/ratings/:slug', Ratings.create);
+router.post('/articles/rate/:slug', Validation.validateRating, Ratings.create);
 
 export default router;
