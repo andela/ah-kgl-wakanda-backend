c9ad4a746f92b5dc11f050be4267adece884aaa1
Karl MUSINGO <karlmusingo@Karls-MacBook-Pro.local>
2019-05-03 11:52:11 +0200

    [FEATURE #165785979] fix hound violations

--- server/routes/api/socialLogin.js
+++ server/routes/api/socialLogin.js
@@ -13,9 +13,14 @@ router.post(
   passport.authenticate('google-token', { session: false }), Users.socialLogin
 );
 
-router.get('/auth/twitter', passport.authenticate('twitter'));
+router.get(
+  '/auth/twitter',
+  passport.authenticate('twitter')
+);
 
-router.get('/auth/twitter/callback',
-  passport.authenticate('twitter', { session: false }), Users.socialLogin);
+router.get(
+  '/auth/twitter/callback',
+  passport.authenticate('twitter', { session: false }), Users.socialLogin
+);
 
 export default router;
