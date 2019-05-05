1a2c90f64b03f84dabaf745cd1f3886908e874b0
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-05-05 19:36:36 +0200

    [#165020135] Add routes for for article searching

---
+++ server/middlewares/validations/searchFilter.js
@@ -0,0 +1,47 @@
+import Joi from 'joi';
+
+/**
+ * @author Mutombo jean-vincent
+ * @class SearchFilter
+ * @description Handle validation for article search filter
+*/
+class SearchFilter {
+  /**
+   * @description Validate rating credentials in the request body
+   * @static
+   * @param {object} req
+   * @param {object} res
+   * @param {callback} next
+   * @returns {void}
+   */
+  static validateFilter(req, res, next) {
+    const schema = {
+      limit: Joi.number()
+        .integer()
+        .min(1)
+        .max(20),
+      offset: Joi.number()
+        .integer()
+        .min(0),
+      author: Joi.number()
+        .string(),
+      title: Joi.number()
+        .string(),
+      keyword: Joi.number()
+        .string(),
+      tags: Joi.number()
+        .string(),
+    };
+
+    const filter = Joi.validate(req.query, schema);
+
+    if (filter.error) {
+      return res.status(400).json({
+        message: filter.error.details[0].message.replace(/[^a-zA-Z0-9 ]/g, ''),
+      });
+    }
+    next();
+  }
+}
+
+export default SearchFilter;
