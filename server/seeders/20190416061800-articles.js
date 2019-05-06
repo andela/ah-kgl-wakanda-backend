a5fa8157c974452a6c81f494db48dd30d81365f1
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-05-06 13:30:51 +0200

    [#165020135] Increase the test coverage

--- server/seeders/20190416061800-articles.js
+++ server/seeders/20190416061800-articles.js
@@ -1,24 +1,86 @@
+const models = require('../models');
+const Article = models.Article;
+const Tags = models.Tags;
 module.exports = {
-	up: (queryInterface) => {
-		return queryInterface.bulkInsert('Articles', [{
-			title: 'How to train your dragon',
-			description: 'Ever wonder how?',
-			body: 'It takes a Jacobian',
-			slug: 'how-to-train-your-dragon',
-			images: ['https://image.jpg'],
-			createdAt: new Date(),
-			updatedAt: new Date()
-		},
-		{
-			title: 'How to train your cat',
-			description: 'Ever wonder how?',
-			body: 'It takes a Jacobian',
-			slug: 'how-to-train-your-cat',
-			images: ['https://image.jpg'],
-			createdAt: new Date(),
-			updatedAt: new Date()
-		}], {});
+	up: async (queryInterface, Sequelize) => {
+		queryInterface.bulkInsert('Articles', [
+			{
+				title: 'How to train your dragon',
+				description: 'Ever wonder how?',
+				body: 'It takes a Jacobian',
+				slug: 'how-to-train-your-dragon',
+				images: ['https://image.jpg'],
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				userId: 1,
+			},
+			{
+				title: 'How to train your cat',
+				description: 'Ever wonder how?',
+				body: 'It takes a Jacobian',
+				slug: 'how-to-train-your-cat',
+				images: ['https://image.jpg'],
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				userId: 1,
+			},
+			{
+				title: 'How to train your dog',
+				description: 'Ever wonder how?',
+				body: 'It takes a Jacobian',
+				slug: 'how-to-train-your-dog',
+				images: ['https://image.jpg'],
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				userId: 1,
+			}
+		], {});
+
+		// get the id from article
+		const article = await Article.findOne({
+			where: {
+				userId: 1,
+				slug: 'how-to-train-your-dog',
+			},
+		});
+
+		//insert the tagName
+		await queryInterface.bulkInsert('Tags', [
+			{
+				tagName: 'cat',
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				tagName: 'dog',
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			}
+		], {});
+
+		// get tags id
+		const tags = await Tags.findAll({});
+
+		// insert the TagArticle
+		return await queryInterface.bulkInsert('TagsArticles', [
+			{
+				articleId: article.id,
+				tagId: tags[0].get().id,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				articleId: article.id,
+				tagId: tags[1].get().id,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			}
+		], {});
 	},
 
-	down: (queryInterface) => { return queryInterface.bulkDelete('Articles', null, {}); }
+	down: async (queryInterface) => {
+		await queryInterface.bulkDelete('TagsArticles', null, {});
+		await queryInterface.bulkDelete('Tags', null, {});
+		await queryInterface.bulkDelete('Articles', null, {});
+	}
 };
