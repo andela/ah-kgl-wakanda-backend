d5c62df2898e288800de2e16b54b7025076bfde6
hadad <hadadus@gmail.com>
2019-05-10 19:12:21 +0200

    [feature #165020142] test the 4 endpoints

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -14,6 +14,7 @@ import permissions from './permissions';
 import admin from './admin';
 import roles from './roles';
 import permissions from './permissions';
+import admin from './admin';
 
 const router = express.Router();
 
@@ -32,5 +33,6 @@ router.use(permissions);
 router.use(admin);
 router.use(roles);
 router.use(permissions);
+router.use(admin);
 
 export default router;
