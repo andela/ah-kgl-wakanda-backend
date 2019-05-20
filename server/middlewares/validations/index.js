40948525c80b4f82b16622c76698898ee39289b1
Grace Lungu <gracelungu@Graces-MacBook-Pro.local>
2019-05-20 15:30:51 +0200

    [Chore #166057202] Group common functionalities

--- server/middlewares/validations/index.js
+++ server/middlewares/validations/index.js
@@ -9,6 +9,12 @@ export default (schema, param = false) => (req, res, next) => {
         message: 'Your password must have at least 6 digits and contain 1 Uppercase, 1 Lowercase, 1 number',
       });
     }
+    if (result.error.details[0].type === 'string.min') {
+      return res.status(400).json({
+        status: 400,
+        message: 'password length must be at least 8 characters long',
+      });
+    }
     return res.status(400).json({
       status: 400,
       message: result.error.details[0].message.replace(/[^a-zA-Z ]/g, ''),
